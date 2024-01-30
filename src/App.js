cebf637040bb19cd13f9995fece311ffb5655349
rishikadsiq <rishika@mediaamp.co.in>
2024-01-30 18:28:07 +0530

    API

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ import Footer from './Footer';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+
+  const API_URL = "http://localhost:3500/items";
+
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items));
-  }, [items])
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const listItems = await response.json();
+        console.log(listItems);
+      } catch (err) {
+        console.log(err.stack)
+      }
+    }
+  }, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
